b7980ccb463297e79f10892e113773c5a250535c
Anna Kurmanova <32227483+annaKurmanova@users.noreply.github.com>
2024-04-10 10:41:55 +0200

    Update amo.php

--- amo.php
+++ amo.php
@@ -3,46 +3,46 @@
 require_once 'access.php';
 alert("Hello, Anya);
 
-$method = "/api/v4/contacts";
+// $method = "/api/v4/contacts";
 
-$headers = [
-    'Content-Type: application/hal+json',
-    'Authorization: Bearer ' . $access_token,
-];
+// $headers = [
+//     'Content-Type: application/hal+json',
+//     'Authorization: Bearer ' . $access_token,
+// ];
 
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_USERAGENT, 'amoCRM-API-client/1.0');
-curl_setopt($curl, CURLOPT_URL, "https://$subdomain.amocrm.ru".$method);
-curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
-curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
-curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($curl, CURLOPT_HEADER, false);
-curl_setopt($curl, CURLOPT_COOKIEFILE, 'amo/cookie.txt');
-curl_setopt($curl, CURLOPT_COOKIEJAR, 'amo/cookie.txt');
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
-$out = curl_exec($curl);
-$code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-$code = (int) $code;
-$errors = [
-    301 => 'Moved permanently.',
-    400 => 'Wrong structure of the array of transmitted data, or invalid identifiers of custom fields.',
-    401 => 'Not Authorized. There is no account information on the server. You need to make a request to another server on the transmitted IP.',
-    403 => 'The account is blocked, for repeatedly exceeding the number of requests per second.',
-    404 => 'Not found.',
-    500 => 'Internal server error.',
-    502 => 'Bad gateway.',
-    503 => 'Service unavailable.'
-];
+// $curl = curl_init();
+// curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+// curl_setopt($curl, CURLOPT_USERAGENT, 'amoCRM-API-client/1.0');
+// curl_setopt($curl, CURLOPT_URL, "https://$subdomain.amocrm.ru".$method);
+// curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+// curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+// curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+// curl_setopt($curl, CURLOPT_HEADER, false);
+// curl_setopt($curl, CURLOPT_COOKIEFILE, 'amo/cookie.txt');
+// curl_setopt($curl, CURLOPT_COOKIEJAR, 'amo/cookie.txt');
+// curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+// curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+// $out = curl_exec($curl);
+// $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+// $code = (int) $code;
+// $errors = [
+//     301 => 'Moved permanently.',
+//     400 => 'Wrong structure of the array of transmitted data, or invalid identifiers of custom fields.',
+//     401 => 'Not Authorized. There is no account information on the server. You need to make a request to another server on the transmitted IP.',
+//     403 => 'The account is blocked, for repeatedly exceeding the number of requests per second.',
+//     404 => 'Not found.',
+//     500 => 'Internal server error.',
+//     502 => 'Bad gateway.',
+//     503 => 'Service unavailable.'
+// ];
 
-if ($code < 200 || $code > 204) die( "Error $code. " . (isset($errors[$code]) ? $errors[$code] : 'Undefined error') );
+// if ($code < 200 || $code > 204) die( "Error $code. " . (isset($errors[$code]) ? $errors[$code] : 'Undefined error') );
 
 
-$Response = json_decode($out, true);
-$Response = $Response['_embedded']['items'];
-$output = 'ID добавленных элементов списков:' . PHP_EOL;
-foreach ($Response as $v)
-    if (is_array($v))
-        $output .= $v['id'] . PHP_EOL;
-return $output;
+// $Response = json_decode($out, true);
+// $Response = $Response['_embedded']['items'];
+// $output = 'ID добавленных элементов списков:' . PHP_EOL;
+// foreach ($Response as $v)
+//     if (is_array($v))
+//         $output .= $v['id'] . PHP_EOL;
+// return $output;
